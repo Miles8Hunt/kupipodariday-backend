63db427a82dba2a92f55b806887df0cb8f544ee8
Ivan Sukhovarov <isukhovarov8@gmail.com>
2024-05-21 14:53:47 +0300

    add: каркас API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getDbConfig } from './config/db.config';
+import { UsersModule } from './users/users.module';
+import { WishesModule } from './wishes/wishes.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
+import { OffersModule } from './offers/offers.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getDbConfig,
+    }),
+    UsersModule,
+    WishesModule,
+    WishlistsModule,
+    OffersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
+
 export class AppModule {}
