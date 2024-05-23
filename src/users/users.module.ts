26d9346d261971e1cb67f5a3c9ff9cbcd2fe0622
Ivan Sukhovarov <isukhovarov8@gmail.com>
2024-05-23 13:14:27 +0300

    add:  импорт моделей в соответствующий модуль

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Wish } from '../wishes/entities/wish.entity';
+import { Offer } from '../offers/entities/offer.entity';
+import { Wishlist } from '../wishlists/entities/wishlist.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, Wish, Offer, Wishlist])],
   controllers: [UsersController],
   providers: [UsersService],
 })
